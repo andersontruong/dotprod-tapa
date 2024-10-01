64e95b91f08ff1af8ea5b085a48086c5a5aedb2e
Anderson Truong <andersonleetruong@gmail.com>
2024-10-01 14:31:16 -0700

    MAC, II-1

--- src/dotprod.cpp
+++ src/dotprod.cpp
@@ -2,41 +2,32 @@
 #include <tapa.h>
 #include <ap_utils.h>
 
-#define ACC_LATENCY 4
+#define N_BINS 8
 
-void Multiply(tapa::istream<float>& v1_q, tapa::istream<float>& v2_q,
-         tapa::ostream<float>& prod_q, uint64_t n) {
-Loop_Multiply: for (uint64_t i = 0; i < n; ++i) {
-        prod_q << (v1_q.read() * v2_q.read());
+void MAC(tapa::istream<float>& v1_q, tapa::istream<float>& v2_q, tapa::mmap<float> dp_out, tapa::ostream<bool>& stop_flag, uint64_t n) {
+    float bins[N_BINS] = { 0.0 };
+    float bins_1[N_BINS] = { 0.0 };
+    #pragma HLS array_partition variable=bins type=complete
+    float total = 0;
+
+Loop_MAC: for (uint64_t i = 0; i < n; ++i) {
+        #pragma HLS loop_tripcount min=1000 max=1000
+        #pragma HLS dependence variable=bins type=inter distance=8 true
+        #pragma HLS dependence variable=bins_1 type=inter distance=8 true
+        #pragma HLS pipeline II=1
+        bins[i % N_BINS] = v1_q.read() * v2_q.read();
+        if (i > 0) {
+            bins_1[(i - 1) % N_BINS] += bins[(i - 1) % N_BINS];
+        }
     }
-}
+    bins_1[(n - 1) % N_BINS] += bins[(n - 1) % N_BINS];
 
-void Accumulate(tapa::istream<float>& prod_q,
-         tapa::mmap<float> prod_out, uint64_t n, tapa::ostream<bool>& stop_flag) {
-    float bins[ACC_LATENCY];
-#pragma HLS array_partition variable = bins complete dim = 1
-    float sum = 0;
-
-Loop_Accumulate_Binning: for (uint64_t i = 0; i < n; ++i) {
-#pragma HLS pipeline II=4
-#pragma HLS unroll factor=4
-        bins[i % ACC_LATENCY] += prod_q.read();
+Loop_Combine: for (uint64_t i = 0; i < N_BINS; ++i) {
+        #pragma HLS unroll
+        total += bins_1[i];
     }
-
-Loop_Accumulate_Join: for (int i = 0; i < ACC_LATENCY; ++i) {
-    #pragma HLS unroll
-        sum += bins[i];
-    }
-
-    prod_out[0] = sum;
+    dp_out[0] = total;
     stop_flag << true;
-}
-
-void Mmap2Stream(tapa::mmap<const float> mmap, uint64_t n,
-                 tapa::ostream<float>& stream) {
-Loop_Mmap2Stream: for (uint64_t i = 0; i < n; ++i) {
-        stream << mmap[i];
-    }
 }
 
 void counter(tapa::mmap<uint64_t> total, tapa::istream<bool>& stop_flag) {
@@ -51,11 +42,19 @@ Loop_Counter: for (;;) {
     total[0] = count;
 }
 
+void Mmap2Stream(tapa::mmap<const float> mmap, uint64_t n,
+                 tapa::ostream<float>& stream) {
+Loop_Mmap2Stream: for (uint64_t i = 0; i < n; ++i) {
+        #pragma HLS pipeline II=1
+        #pragma HLS loop_tripcount min=1000 max=1000
+        stream << mmap[i];
+    }
+}
+
 void DotProd(tapa::mmap<const float> v1, tapa::mmap<const float> v2,
              tapa::mmap<float> prod_out, uint64_t n, tapa::mmap<uint64_t> total) {
     tapa::stream<float> v1_q("v1");
     tapa::stream<float> v2_q("v2");
-    tapa::stream<float> prod_q("prod");
 
     tapa::stream<bool> stop_flag("stop");
 
@@ -63,6 +62,6 @@ void DotProd(tapa::mmap<const float> v1, tapa::mmap<const float> v2,
         .invoke(counter, total, stop_flag)
         .invoke(Mmap2Stream, v1, n, v1_q)
         .invoke(Mmap2Stream, v2, n, v2_q)
-        .invoke(Multiply, v1_q, v2_q, prod_q, n)
-        .invoke(Accumulate, prod_q, prod_out, n, stop_flag);
+        .invoke(MAC, v1_q, v2_q, prod_out, stop_flag, n)
+    ;
 }
